58ee801976df6df2893084a74d345ac7537ff412
Shaibu Zachariyya <maddiiana@gmail.com>
2014-09-10 00:59:23 -0800

    new

--- scripts/html5audio.js
+++ scripts/html5audio.js
@@ -22,7 +22,7 @@ function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
 
-var myaudioURL = 'http://206.217.201.137:8001/;';
+var myaudioURL = 'http://206.217.201.137:8001;';
 var myaudio = new Audio(myaudioURL);
 var isPlaying = false;
 var readyStateInterval = null;
